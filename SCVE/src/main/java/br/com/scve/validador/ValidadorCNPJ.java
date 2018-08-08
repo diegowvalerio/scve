8e99b01bd8c57ca59f320b477fcb110d931b968b
Diego <die_gow@hotmail.com>
2018-08-07 21:22:52 -0300

    valida cpf/cnpj

--- SCVE/src/main/java/br/com/scve/validador/ValidadorCNPJ.java
+++ SCVE/src/main/java/br/com/scve/validador/ValidadorCNPJ.java
@@ -7,13 +7,13 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-@FacesValidator("cpfValidador")
-public class ValidadorCPF implements Validator{
+@FacesValidator("cnpjValidador")
+public class ValidadorCNPJ implements Validator{
 
 	@Override
 	public void validate(FacesContext fc, UIComponent ui1, Object valor) throws ValidatorException {
-		if(!CPF.validar(valor.toString())){
-			FacesMessage msg = new FacesMessage("CPF Inválido","Por favor, informe um valor válido!");
+		if(!CNPJ.isCNPJ(valor.toString())){
+			FacesMessage msg = new FacesMessage("CNPJ Inválido","Por favor, informe um valor válido!");
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 			throw new ValidatorException(msg);
 		}
